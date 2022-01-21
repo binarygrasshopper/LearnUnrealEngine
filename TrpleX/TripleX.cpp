a713a8219d6b57e86f1bc0765961473dbc0be5bc
binarygrasshopper <dwbgithub@gmail.com>
2022-01-21 18:39:09 -0500

    Initial Game Check-in

---
+++ TrpleX/TripleX.cpp
@@ -0,0 +1,14 @@
+// TrpleX.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "You a screat agent breaking into a secure server room...";
+    std::cout << std::endl;
+    std::cout << "You need to enter the correct codes to continue...";
+    return 0;
+}
+
+
